749842ac806840e6b1c68d184d9820b112a3a16e
SebastianNiechcial <sebastian-niechcial@wp.pl>
2024-04-05 23:14:33 +0200

    Stowrzenie Layout + przycisk

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+interface CardInfo {
+  header: string;
+  location: string;
+  message: string;
+  photo: string;
+}
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
-
+  items = {
+    header: 'podróż',
+    location: 'Włochy',
+    message: 'La Prima',
+    photo: 'https://ionic-docs-demo-v7.vercel.app/assets/madison.jpg',
+  };
+  addPlan(Plan: CardInfo) {
+    console.log(
+      `location: ${Plan.location} message: ${Plan.message} photo: ${Plan.photo}`
+    );
+  }
 }
